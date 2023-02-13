841a428489780064fce18c083314d7cc5d2f48bc
Carla Hentschel <carlarhentschel@gmail.com>
2023-02-13 20:52:12 -0300

    adicionado edição nos recados

--- recados.js
+++ recados.js
@@ -1,9 +1,8 @@
-/* const listaRecados = []; */
-
+let indiceAtualizado = -1 
 const usuarioLogado = buscarDadosDoLocalStorage('usuarioLogado');
-
 const meuModalCriarRecado = new bootstrap.Modal('#criar-recado');
 const meuModalEditar = new bootstrap.Modal('#modal-editar');
+const meuModalExcluir = new bootstrap.Modal('#modal-excluir')
 
 document.addEventListener('DOMContentLoaded', () => {
     if(!usuarioLogado.email) {
@@ -14,7 +13,9 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 const formRecado = document.getElementById('form-recado');
-const tbody = document.getElementById('registros')
+const formEditar = document.getElementById('form-editar');
+const tbody = document.getElementById('registros');
+
 
 formRecado.addEventListener('submit', (evento) => {
     evento.preventDefault()
@@ -46,24 +47,51 @@ formRecado.addEventListener('submit', (evento) => {
     console.log(listaRecados);
 })
 
+formEditar.addEventListener('submit', (evento) => {
+    evento.preventDefault()
+
+    if(!formEditar.checkValidity()) {
+        formEditar.classList.add('was-validated')
+        return
+    }
+
+    const tarefaAtualizada = document.getElementById('editar-tarefa').value;
+    const detalhamentoAtualizado = document.getElementById('editar-detalhamento').value;
+
+    usuarioLogado.recados[indiceAtualizado].tarefa = tarefaAtualizada;
+    usuarioLogado.recados[indiceAtualizado].detalhamento = detalhamentoAtualizado;
+
+    guardarNoLocalStorage('usuarioLogado', usuarioLogado);
+
+    montarRegistrosNoHTML();
+
+    meuModalEditar.hide();
+    formEditar.classList.remove('was-validated');
+    formEditar.reset();
+
+    // mostrarAlerta('success', 'Recado atualizado com sucesso!')
+
+    indiceAtualizado= -1
+
+})
+
 function montarRegistrosNoHTML() {
+    
     tbody.innerHTML = '';
 
     usuarioLogado.recados.forEach((valor, index) => {
         tbody.innerHTML += `
-            <tr id="${index}">
-                <td>${index +1} </td>
+            <tr id="${valor.id}">
+                <td>${index+1} </td>
                 <td>${valor.tarefa}</td>
                 <td>${valor.detalhamento}</td>
                 <td>
-                    <button id="apagar" class="btn btn-success fs-4" aria-label="Editar" onclick="apagarRecado(${index})">
-                        <i class="bi bi-trash3"></i>
-                        Apagar
-                    </button>
-                    <button id="editar" class="btn btn-danger fs-4" type="button" onclick="prepararEdicao(${index})" data-bs-toggle="modal" data-bs-target="#modal-editar" >
-                        <i class="bi bi-pencil-square"></i>
-                        Editar
+                    <button class="btn btn-success m-1 fs-4" aria-label="Editar" onclick="prepararEdicao(${index})">
+                        <i class="bi bi-pencil-square"></i>                       
                     </button> 
+                    <button class="btn btn-danger m-1 fs-4" aria-label="Apagar" onclick="prepararExclusao(${index}, ${valor.id})">
+                        <i class="bi bi-trash3"></i>                       
+                    </button>
                 </td>
             </tr>
         `
@@ -91,45 +119,49 @@ function buscarDadosDoLocalStorage(chave) {
     }
 }
 
-function apagarRecado(indice) {
-    let confirma = window.confirm('Deseja realmente apagar esta tarefa?');
+function prepararExclusao(index, id) {
+    meuModalExcluir.show();
 
-    if(confirma){
-        usuarioLogado.recados.splice(indice, 1)
+    const botaoExcluir = document.getElementById('btn-delete');
 
-        guardarNoLocalStorage('usuarioLogado', usuarioLogado)
-
-        const tr = document.getElementById(indice)
-        tr.remove()
-        montarRegistrosNoHTML()
-    }
+    botaoExcluir.setAttribute('onclick', `apagarRecado(${index}, ${id})`);
 }
 
-function prepararEdicao(indice) {
+function apagarRecado(index, id) {
+    console.log(index);
+
+    usuarioLogado.recados.splice(index, 1)
+
+    guardarNoLocalStorage('usuarioLogado', usuarioLogado)
+
+    const tr = document.getElementById(id)
+    tr.remove()
+
+    montarRegistrosNoHTML()
+
+    meuModalExcluir.hide();
+
+    // mostrar alerta com toast
+
+    // mostrarAlerta('success', 'Recado excluído com sucesso!')
+
+}
+
+function prepararEdicao(index, id) {
+
+    const recadoAtualizar = usuarioLogado.recados[index]
+
+    console.log(recadoAtualizar);
+
+    meuModalEditar.show();
     
     const inputEditarTarefa = document.getElementById("editar-tarefa");
     const inputEditarDetalhamento = document.getElementById("editar-detalhamento");
 
-    inputEditarTarefa.value = usuarioLogado.recados[indice].tarefa
-    inputEditarDetalhamento.value = usuarioLogado.recados[indice].detalhamento
+    inputEditarTarefa.value = usuarioLogado.recados[index].tarefa
+    inputEditarDetalhamento.value = usuarioLogado.recados[index].detalhamento
 
-    const formularioEditar = document.getElementById('formulario-editar-recados')
-    formularioEditar.addEventListener('submit', (evento) => {
-        evento.preventDefault()
-
-        // atualizar a lista de recados
-        usuarioLogado.recados[indice].tarefa = inputEditarTarefa.value
-        usuarioLogado.recados[indice].detalhamento = inputEditarDetalhamento.value
-        console.log(usuarioLogado.recados[indice])
-
-        // atualizar o localStorage
-        guardarNoLocalStorage('usuarioLogado', usuarioLogado)
-
-        // atualizar o html
-        montarRegistrosNoHTML()
-
-        meuModalEditar.hide()
-    })
+    indiceAtualizado = index
 }
 
 function sairDaAplicacao() {
